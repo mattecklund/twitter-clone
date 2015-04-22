c6914da826087e3b42b2041f838c2d2628e38f22
Matthew Ecklund <mecklund@ldschurch.org>
2015-04-22 14:01:33 -0600

    finished

--- js/animations.js
+++ js/animations.js
@@ -6,9 +6,11 @@ $(document).ready(function() {
 	var stream = $('#stream');
 	var textColor = charCount.css("color");
 	var maxChars = 140;
+	var tweetActions = $('.tweet-actions');
 
 	tweetButton.hide();
 	charCount.hide();
+	$('.tweet-actions').hide();
 	var tweetComposeHeight = tweetCompose.height();
 	tweetCompose.on('click', function() {
 		tweetButton.show();
@@ -45,11 +47,30 @@ $(document).ready(function() {
 	});
 
 	tweetButton.on('click', function(){
-		console.log('Testing if onClick is working')
-		console.log(tweetCompose);
-		var newTweet = "<div class=\"tweet\"><div class=\"content\"><img class=\"avatar\" src=\"img/alagoon.jpg\" /><strong class=\"fullname\">Matthew Ecklund</strong><span class=\"username\">@myself</span><p class=\"tweet-text\">";
-		stream.prepend(newTweet + tweetCompose.val() + "</p></div></div>");
-	})
+		// console.log('Testing if onClick is working')
+		// console.log(tweetCompose);
+		var beforeTweet = "<div class=\"tweet\"><div class=\"content\"><img class=\"avatar\" src=\"img/alagoon.jpg\" /><strong class=\"fullname\">Matthew Ecklund</strong><span class=\"username\">@myself</span><p class=\"tweet-text\">";
+		var afterTweet = "</p><div class=\"tweet-actions\"><ul><li><span class=\"icon action-reply\"></span> Reply</li><li><span class=\"icon action-retweet\"></span> Retweet</li><li><span class=\"icon action-favorite\"></span> Favorite</li><li><span class=\"icon action-more\"></span> More</li></ul></div><div class=\"stats\"><div class=\"retweets\"><p class=\"num-retweets\">10</p><p>RETWEETS</p></div><div class=\"favorites\"><p class=\"num-favorites\">2</p><p>FAVORITES</p></div><div class=\"users-interact\"><div><img src=\"img/alagoon.jpg\" /><img src=\"img/vklimenko.jpg\" /></div></div><div class=\"time\">1:04 PM - 19 Sep 13</div></div><div class=\"reply\"><img class=\"avatar\" src=\"img/alagoon.jpg\" /><textarea class=\"tweet-compose\" placeholder=\"Reply to @myself\"/></textarea></div></div></div>";
+		stream.prepend(beforeTweet + tweetCompose.val() + afterTweet);
+		// console.log('prepending new Tweet');
+		//remove value of tweetCompose from input box
+		tweetHover();
+		$('.tweet-actions').hide();
+	});
+	
+	var tweetHover = function(){
+		$('.tweet').hover(
+			function() {
+				// console.log('hovering over tweet class');
+				$('.tweet-actions', this).show();
+				// $(this).child('.tweet-actions').show();
+			}, function() {
+				// console.log('leaving element');
+				$('.tweet-actions', this).hide();
+			}
+		)
+	};
+	tweetHover();
 })
 
 
